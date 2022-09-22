f5493a826b6f3ecc42f132ed8a1d0f10bd9b6e63
cccczl <cccczl@hotmail.com>
2022-09-22 20:00:10 +0800

    day01 完全中文版

--- day01/main.py
+++ day01/main.py
@@ -1,39 +1,36 @@
-# Band Name Generator
-# 乐队名称生成器
-print("欢迎使用乐队名称生成器。")
 
-# initialize the variables as empty strings
+# 问候语生成器
+print("欢迎使用问候生成器。")
+
 # 将变量初始化为空字符
-city = ""
-pet_name = ""
+chengshi = ""
+xing_ming = ""
 
-# simple check to make sure the user has entered something
-# 简单的检查，确保用户已经输入了内容
+
+# 简单的检查，确保chengshi已经输入了内容
 
 while True:
-    print("What's the name of the city you grew up in?")
-    city = input("> ")
-    # if there's no input, ask again
+    print("请输入你来自那个城市！")
+    chengshi = input("> ")
     # 如果没有输入，再问一次
-    if city == "":
-        print("You haven't entered anything. Please try again.")
-    # if there's any input at all, break out of the loop
+    if chengshi == "":
+        print("你没有输入任何东西. 请检查.")
     # 如果有输入，就跳出循环
     else:
         break
 
-# do the same for the pet name
-# 接受乐队名输入检查，和上段代码同样处理
+# 简单的检查，确保xing_ming已经输入了内容
 while True:
-    print("What's your pet's name?")
-    pet_name = input("> ")
-    if pet_name == "":
-        print("You haven't entered anything. Please try again.")
+    print("请输入你的名字！")
+    xing_ming = input("> ")
+    # 如果没有输入，再问一次
+    if xing_ming == "":
+        print("你没有输入任何东西. 请检查.")
+    # 如果有输入，就跳出循环
     else:
         break
 
-# output using f-strings makes the code much more readable
 # 使用f-strings的输出使代码可读性更强
-print(f"Your band name could be {city} {pet_name}.")
-print("Your band name could be {} {}.".format(city,pet_name))
-print("Your band name could be "+city, pet_name+". ")
+print(f"欢迎你，来自{chengshi}的{xing_ming}.")
+print("欢迎你，来自{}的{}.".format(chengshi,xing_ming))
+print("欢迎你，来自"+ chengshi + "的" + xing_ming + ". ")
