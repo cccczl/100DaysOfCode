e74e310bd5d895c33b5d6f7697dcebedf86c9ebc
cccczl <cccczl@hotmail.com>
2022-10-25 18:41:49 +0800

    添加函数

---
+++ day15/main copy.py
@@ -0,0 +1,46 @@
+#TODO2 处理菜单问题
+MENU = {
+    "natie": {
+        "peiliao": {"shui": 50, "kafei": 18, }, "jiage": 15,
+    },
+    "kabuqiluo": {
+        "peiliao": {"shui": 100, "kafei": 25, "niunai": 100}, "jiage": 25
+    },
+    "maqiduo": {
+        "peiliao": {"shui": 100, "kafei": 30, "niunai": 100}, "jiage": 30
+    }
+}
+
+ying_ye_shou_ru = 0    #营业款
+
+#是否开机营业
+shi_fou_kai_ji = True
+
+#TODO 处理库存问题
+#库存资源
+ku_cun = {"shui": 10, "kafei": 500, "niunai": 500}
+
+def dui_bi_shu_ru_ku_cun(yong_hu_pei_liao):
+    for item in yong_hu_pei_liao:
+        if yong_hu_pei_liao[item] >= ku_cun[item]:
+            print(f"咖啡机中{item}不够了。")
+            return False
+    return True
+
+
+def jie_shu_an():
+    
+
+while shi_fou_kai_ji:
+    yong_hu_shu_ru = input("请你输入你喜欢的咖啡,natie/kabuqiluo/maqiduo:>").lower()
+    if yong_hu_shu_ru == "caxun":
+       print(f"shui: {ku_cun['shui']}ml")
+       print(f"kafei: {ku_cun['kafei']}g")
+       print(f"niunai: {ku_cun['niunai']}ml")
+       print(f"营业收入: {ying_ye_shou_ru}¥")
+    else:
+        zhi_zuo = MENU[yong_hu_shu_ru]
+        print(zhi_zuo)
+        if dui_bi_shu_ru_ku_cun(zhi_zuo["peiliao"]):
+            print(dui_bi_shu_ru_ku_cun(zhi_zuo["peiliao"]))
+        
