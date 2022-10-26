e0e4f6d073d9e25eb176b868c605b42a281ca16b
cccczl <cccczl@hotmail.com>
2022-10-26 18:15:49 +0800

    函数修正

--- day15/main.py
+++ day15/main.py
@@ -18,8 +18,23 @@ shi_fou_kai_ji = True
 
 #TODO 处理库存问题
 #库存资源
-ku_cun = {"shui": 1000, "kafei": 300, "niunai": 1000}
+ku_cun = {"shui": 10, "kafei": 500, "niunai": 500}
 
+def dui_bi_shu_ru_ku_cun(yong_hu_pei_liao):
+    for item in yong_hu_pei_liao:
+        if yong_hu_pei_liao[item] >= ku_cun[item]:
+            print(f"咖啡机中{item}不够了。")
+            return False
+    return True
+
+
+def jie_shu_an():
+    print("请你投入硬币。")
+    tou_ru_lei_ji = int(input("请你投入1元硬币")) * 1
+    tou_ru_lei_ji += int(input("请你投入5角硬币")) * 0.5
+    tou_ru_lei_ji += int(input("请你投入2角硬币")) * 0.2
+    tou_ru_lei_ji += int(input("请你投入1角硬币")) * 0.1
+    return tou_ru_lei_ji
 
 
 while shi_fou_kai_ji:
@@ -29,11 +44,9 @@ while shi_fou_kai_ji:
        print(f"kafei: {ku_cun['kafei']}g")
        print(f"niunai: {ku_cun['niunai']}ml")
        print(f"营业收入: {ying_ye_shou_ru}¥")
-    elif yong_hu_shu_ru == "natie":
-        pass
-    elif yong_hu_shu_ru == "kabuqiluo":
-        pass
-    elif yong_hu_shu_ru == "maqiduo":
-        pass
     else:
-        pass
+        zhi_zuo = MENU[yong_hu_shu_ru]
+        print(zhi_zuo)
+        if dui_bi_shu_ru_ku_cun(zhi_zuo["peiliao"]):
+            # jie_suan = jie_shu_an()
+            # print(jie_shu - zhi_zuo["jiage"] )        
