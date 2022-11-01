8ccffb9c92b09daa313273adb476c45c491ab598
cccczl <cccczl@hotmail.com>
2022-11-01 18:33:55 +0800

    修正英文

--- day15/main.py
+++ day15/main.py
@@ -3,14 +3,14 @@ from hashlib import new
 
 
 MENU = {
-    "natie": {
-        "formula": {"shui": 50, "kafei": 18, }, "jiage": 15,
+    "latte": {
+        "formula": {"water": 50, "coffee": 18, }, "price": 15,
     },
-    "kabuqiluo": {
-        "formula": {"shui": 100, "kafei": 25, "niunai": 100}, "jiage": 25
+    "cappuccino": {
+        "formula": {"water": 100, "coffee": 25, "milk": 100}, "price": 25
     },
-    "maqiduo": {
-        "formula": {"shui": 100, "kafei": 30, "niunai": 100}, "jiage": 30
+    "macchiatto": {
+        "formula": {"water": 100, "coffee": 30, "milk": 100}, "price": 30
     }
 }
 
@@ -22,11 +22,11 @@ purse = 0
 is_open = True
 
 # 库存资源
-stock = {"shui": 1000, "kafei": 500, "niunai": 500}
+stock = {"water": 1000, "coffee": 500, "milk": 500}
 
 # 自检函数
 
-def make_caffes_test(formulas):
+def make_coffees_test(formulas):
     for item in formulas:
         if formulas[item] >= stock[item]:
             print(f"咖啡机中{item}不够了。")
@@ -42,21 +42,21 @@ def coin():
 
 
 while is_open:
-    user_inprt = input("请你输入你喜欢的咖啡,natie/kabuqiluo/maqiduo:>").lower()
-    if user_inprt == "caxun":
-        print(f"shui: {stock['shui']}ml")
-        print(f"kafei: {stock['kafei']}g")
-        print(f"niunai: {stock['niunai']}ml")
+    user_inprt = input("请你输入你喜欢的咖啡,latte/cappuccino/macchiatto:>").lower()
+    if user_inprt == "inquire":
+        print(f"shui: {stock['water']}ml")
+        print(f"kafei: {stock['coffee']}g")
+        print(f"niunai: {stock['milk']}ml")
         print(f"营业收入: {purse}¥")
     else:
-        make_caffes = MENU[user_inprt]
-        print(make_caffes)
-        if make_caffes_test(make_caffes["formula"]):
+        make_coffees = MENU[user_inprt]
+        print(make_coffees)
+        if make_coffees_test(make_coffees["formula"]):
             # coin()
             newcoin = coin()
-            if newcoin < make_caffes["jiage"]:            
+            if newcoin < make_coffees["price"]:            
                 print("你的钱不够")
-            elif newcoin == make_caffes["jiage"]:
+            elif newcoin == make_coffees["price"]:
                 print("咖啡制作中")
             else:
                 print("找零中完成，咖啡制作中")
